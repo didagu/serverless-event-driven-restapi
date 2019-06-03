d53f98d948bee9cd4c33bfd322f37df0182dcef9
Dominic Idagu <didagou@gmail.com>
2019-06-03 18:39:19 +0100

    Add uuidv1 to location while saving it to DB

--- locations-api/src/handlers/locations/add.js
+++ locations-api/src/handlers/locations/add.js
@@ -2,20 +2,25 @@
 
 require('dotenv/config')
 
-const { LocationRepository } = require('../../repositories/location.repository')
+const { LocationRepository } = require('../../repositories/location.repository') 
 const { parseWith } = require('../../utils/request.util')
 const { withStatusCode } = require('../../utils/response.util')
 const { DocClientWithEnvVar } = require('../../dynamodb.factory')
+const uuidv1 = require('uuid/v1')
 
 const docClient = DocClientWithEnvVar(process.env)
 const repository = new LocationRepository(docClient)
-const created = withStatusCode(201)
+const created = withStatusCode(204)
 const parseJson = parseWith(JSON.parse)
 
-exports.handler = async (event) => {
+exports.handler =  async (event) => {
+
   console.log(event)
   const { body } = event
   const location = parseJson(body)
+  location = Object.assign({}, location, {
+    "id" : uuidv1()
+  })
   await repository.put(location)
   return created()
 }
