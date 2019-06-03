a77f8a19c39393699894e5555ff9aa0421d2f66c
Dominic Idagu <didagou@gmail.com>
2019-06-03 18:38:18 +0100

    Add calculated distance from location to office using Harvesine library to location data

--- locations-api/src/handlers/locations/get.js
+++ locations-api/src/handlers/locations/get.js
@@ -5,11 +5,14 @@ require('dotenv/config')
 const { LocationRepository } = require('../../repositories/location.repository')
 const { withStatusCode } = require('../../utils/response.util')
 const { DocClientWithEnvVar } = require('../../dynamodb.factory')
+const haversine = require('haversine-distance')
 
 const docClient = DocClientWithEnvVar(process.env)
 const repository = new LocationRepository(docClient)
 const ok = withStatusCode(200, JSON.stringify)
 const notFound = withStatusCode(404)
+
+const officeCoordinates = { lat: 52.502931, lng: 13.408249 }
 
 exports.handler = async (event) => {
   const { id } = event.pathParameters
@@ -18,6 +21,11 @@ exports.handler = async (event) => {
   if (!location) {
     return notFound()
   }
+
+  let locationCoordinates = { lat: location.Item.latitude,lon: location.Item.longitude}
+  
+  //Calculated distance to the office in Km
+  location.Item.caluclatedDistanceToOffice = haversine(officeCoordinates, locationCoordinates)
 
   return ok(location)
 }
