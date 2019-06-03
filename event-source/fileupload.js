d23f7733a55403850acea7d79cc52d9364388304
Dominic Idagu <didagou@gmail.com>
2019-06-03 18:41:13 +0100

    Change file name from handler.js to fileupload.js

---
+++ event-source/fileupload.js
@@ -0,0 +1,58 @@
+'use strict';
+
+require('dotenv/config');
+
+const multipart = require('aws-lambda-multipart-parser')
+
+const SDK = require('@serverless/event-gateway-sdk')
+
+// initialize event gateway
+const eventGateway = new SDK({
+  url: process.env.EVENT_GATEWAY_URL
+});
+
+exports.handler = async (event) => {
+
+  const uploadedFile = multipart.parse(event, false)
+
+  if(uploadedFile.file.contentType !== 'application/json') {
+    return {
+      statusCode:400,
+      body: JSON.stringify({
+        message: "The uploaded file must be in JSON format"
+      })
+    }
+  }
+
+  const {latitude, longitude} = JSON.parse(uploadedFile.file.content)
+  const fileName = uploadedFile.file.filename
+  const data = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "location_name": fileName.substr(0, fileName.lastIndexOf('.'))
+  }
+
+  // Emit file.uploaded event
+  let emitEvent = await eventGateway
+    .emit({
+      eventID: '1',
+      eventType: 'file.uploaded',
+      cloudEventsVersion: '0.1',
+      source: '/event-source',
+      contentType: 'application/json',
+      data: data
+    })
+  console.log(emitEvent)
+
+  // emitEvent
+  //   .then(res => console.log(res))
+  //   .catch(err => console.log('error from event emittion:', err))
+
+  return {
+    statusCode: emitEvent.status,
+    body: JSON.stringify({
+      message: emitEvent.statusText,
+      data: emitEvent
+    })
+  }
+}
